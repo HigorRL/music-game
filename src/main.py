2a9743ef39e9dc67f5ad79e99959197462413cdf
Seu nome para exibiçãoHigorRL <higor_librelato_2000@hotmail.com>
2024-03-21 16:27:47 -0300

    adicionando novas funcionalidades

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ menu_options = ["NOVO JOGO", "CARREGAR JOGO", "CONFIGURAÇÕES", "SOBRE", "SAIR"
 selected_option = 0
 
 # Config de volume da música
-music_volume = 0.2
+music_volume = 0.1
 volume_increment = 0.1
 
 # Inicia a música de fundo do menu
@@ -60,7 +60,7 @@ def draw_menu():
 def draw_settings_menu(selected_option):
     screen.fill(BLACK)  # Limpa a tela com fundo preto
     font = pygame.font.SysFont('arial', 35, bold=True)
-    options = ["AJUSTAR VOLUME", "VOLTAR"]
+    options = ["AJUSTAR VOLUME", "ALTERAR RESOLUÇÃO", "VOLTAR"]
     spacing = 44
     menu_height = len(options) * spacing
     menu_top = (HEIGHT - (menu_height + 65))
@@ -75,7 +75,7 @@ def draw_settings_menu(selected_option):
 
 
 def handle_settings_menu():
-    global selected_option
+    global selected_option, WIDTH, HEIGHT
     selected_option = 0
     while True:
         draw_settings_menu(selected_option)
@@ -87,15 +87,75 @@ def handle_settings_menu():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     return
-                elif event.key == K_DOWN:
+                elif event.key == K_DOWN or event.key == K_s:
                     selected_option = (selected_option + 1) % 2
-                elif event.key == K_UP:
+                elif event.key == K_UP or event.key == K_w:
                     selected_option = (selected_option - 1) % 2
                 elif event.key == K_RETURN:
                     if selected_option == 0:  # Ajustar o volume
                         adjust_volume()
-                    elif selected_option == 1:  # Voltar ao menu principal
+                    elif selected_option == 1:  # Alterar resolução
+                        change_resolution()
+                    elif selected_option == 2:  # Volta para o menu principal
                         return
+
+# Função para alterar a resolução
+
+
+def change_resolution():
+    global WIDTH, HEIGHT, screen, menu_background
+    resolutions = [(800, 600), (1024, 768), (1280, 720),
+                   (1366, 768), (1920, 1080)]
+    resolution_index = 0
+    while True:
+        screen.fill(BLACK)
+        font = pygame.font.SysFont('arial', 35, bold=True)
+        spacing = 44
+        menu_height = len(resolutions) * spacing
+        menu_top = (HEIGHT - (menu_height + 65))
+        for i, res in enumerate(resolutions):
+            text = font.render(
+                f"{res[0]}x{res[1]}", True, WHITE if i == resolution_index else (80, 80, 80))
+            # Centraliza o texto na tela
+            text_rect = text.get_rect(
+                center=(WIDTH / 2, menu_top + i * spacing))
+            screen.blit(text, text_rect)
+        pygame.display.update()
+
+        # Trata os eventos do teclado
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    return
+                elif event.key == K_DOWN:
+                    resolution_index = (
+                        resolution_index + 1) % len(resolutions)
+                elif event.key == K_UP:
+                    resolution_index = (
+                        resolution_index - 1) % len(resolutions)
+                elif event.key == K_RETURN:
+                    # Altera a resolução
+                    old_width, old_height = WIDTH, HEIGHT
+                    WIDTH, HEIGHT = resolutions[resolution_index]
+
+                    # Redimensiona a imagem de fundo
+                    menu_background = pygame.transform.scale(
+                        menu_background, (WIDTH, HEIGHT))
+
+                    # Atualiza a posição dos elementos na tela
+                    menu_top = (HEIGHT - (menu_height + 65))
+                    for i, _ in enumerate(resolutions):
+                        text_rect.center = (WIDTH / 2, menu_top + i * spacing)
+
+                    # Atualiza a janela com a nova resolução
+                    pygame.display.set_mode(
+                        (WIDTH, HEIGHT), pygame.RESIZABLE)
+                    pygame.display.set_caption('The Music Saga')
+
+                    return
 
 # Função para ajustar o volume
 
@@ -129,15 +189,15 @@ def handle_events():
             pygame.quit()
             exit()
         if event.type == KEYDOWN:
-            if event.key == K_s:
+            if event.key == K_DOWN or event.key == K_s:
                 selected_option = (selected_option + 1) % len(menu_options)
-            elif event.key == K_w:
+            elif event.key == K_UP or event.key == K_w:
                 selected_option = (selected_option - 1) % len(menu_options)
             elif event.key == K_RETURN:
                 if selected_option == len(menu_options) - 1:
                     pygame.quit()
                     exit()
-                elif selected_option == 2:  # Configurações
+                elif selected_option == 2:  # Seleciona configurações
                     handle_settings_menu()
 
 # Loop principal do jogo
